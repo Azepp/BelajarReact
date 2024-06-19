ba8c8225afd360f57e1f8a3ac1f9a5fa525a524a
Azepp <zidanathallahwinata@gmail.com>
2024-06-19 08:55:03 +0700

    FixInput

--- src/Components/Elements/Input/Input.jsx
+++ src/Components/Elements/Input/Input.jsx
@@ -1,8 +1,6 @@
 function Input(props) {
   const { type, placeholder, name } = props;
-  return (
-    <input id={name} type={type} name={name} className="w-full py-2 px-3 rounded-md border-slate-500 focus:ring-1 focus:ring-blue-500 focus:outline-none border border-opacity-50 text-sm placeholder:opacity-75" placeholder={placeholder} />
-  );
+  return <input id={name} type={type} name={name} className="w-full py-2 px-3 rounded-md border-slate-500 focus:ring-1 focus:ring-blue-500 focus:outline-none border border-opacity-50 text-sm bg-transparent" placeholder={placeholder} />;
 }
 
 export default Input;
