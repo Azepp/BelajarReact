198e6896a9031335bd49e3814aae67491fcbc75d
Zidane Athallah Winata <zidanathallahwinata@gmail.com>
2024-07-16 21:27:51 +0700

    PostApi

--- src/Components/Fragments/FormLogin.jsx
+++ src/Components/Fragments/FormLogin.jsx
@@ -1,28 +1,45 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import Button from "../Elements/Button/Button";
 import InputForm from "../Elements/Input/InputForm";
-import Remember from "../Elements/Input/Remember";
+import { login } from "../../services/auth.service";
 
 function FormLogin() {
+  const [loginFailed, setLoginFailed] = useState("");
+
   const handleLogin = (event) => {
     event.preventDefault();
-    localStorage.setItem("email", event.target.email.value);
-    localStorage.setItem("password", event.target.password.value);
-    window.location.href = "/products";
+    // localStorage.setItem("email", event.target.email.value);
+    // localStorage.setItem("password", event.target.password.value);
+    const data = {
+      username : event.target.username.value,
+      password : event.target.password.value,
+    }
+
+    login(data, (status, res) => {
+      if (status) {
+        localStorage.setItem("token", res);
+        window.location.href = "/products";
+      } else {
+        setLoginFailed(res.response.data);
+        console.log(res.response.data);
+      }
+    })
   };
 
-  const emailRef = useRef(null);
+  const usernameRef = useRef(null);
 
   useEffect (() => {
-    emailRef.current.focus();
+    usernameRef.current.focus();
   }, []);
 
   return (
     <>
       <form onSubmit={handleLogin}>
-        <InputForm label="Email" type="email" placeholder="Enter your email" name="email" ref={emailRef}></InputForm>
+        <InputForm label="Username" type="text" placeholder="Enter your username" name="username" ref={usernameRef}></InputForm>
         <InputForm label="Password" type="password" placeholder="Enter your password" name="password"></InputForm>
-        <Remember></Remember>
+
+        <p className="my-4 text-red-500 text-center capitalize">{loginFailed}</p>
+
         <Button variant="bg-blue-500" text="white" hover="bg-blue-700" type="submit" >
           Login
         </Button>
--- src/Components/Fragments/Navbar.jsx
+++ src/Components/Fragments/Navbar.jsx
@@ -1,17 +1,29 @@
-import { useLocation, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import Button from "../Elements/Button/Button";
+import { getUsername } from "../../services/auth.service";
 
 function NavbarAfter() {
-  const email = localStorage.getItem("email");
+  const [username, setUsername] = useState("");
+  const location = useLocation();
+
   const handleLogout = () => {
-    localStorage.removeItem("email");
-    localStorage.removeItem("password");
+    localStorage.removeItem("token");
     window.location.href = "/";
   };
 
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+       setUsername(getUsername(token));
+    } else {
+      window.location.href = "/login";
+    }
+  }, []);
+
   return (
     <div className="flex w-full justify-between gap-4 items-center bg-blue-500 py-4 px-12 text-white">
-      <h1>{email}</h1>
+      <h1>{username}</h1>
       {location.pathname !== "/" && (
         <Button variant="bg-red-500" text="white" size="w-1/2" onClick={handleLogout}>
           Logout
@@ -21,43 +33,4 @@ function NavbarAfter() {
   );
 }
 
-function NavbarBefore() {
-  const navigate = useNavigate();
-
-  const handleLogin = () => {
-    navigate("/login");
-  };
-
-  const handleRegis = () => {
-    navigate("/register");
-  };
-
-  return (
-    <div className="flex w-full justify-between gap-4 items-center bg-blue-500 py-4 px-12 text-white fixed">
-      <h1 className="text-2xl font-bold">ShoesSell</h1>
-      <ul className="flex gap-2">
-        <li>Home</li>
-        <li>Product</li>
-        <li>Home</li>
-        <li>Home</li>
-      </ul>
-
-      <div className="flex gap-2">
-        <Button variant="outline outline-2" text="white" size="w-1/2" onClick={handleLogin}>
-          Login
-        </Button>
-        <Button variant="bg-orange-500" text="white" size="w-1/2" onClick={handleRegis}>
-          Register
-        </Button>
-      </div>
-    </div>
-  );
-}
-
-function Navbar() {
-  const location = useLocation();
-
-  return location.pathname === "/" ? <NavbarBefore /> : <NavbarAfter />;
-}
-
-export default Navbar;
+export default NavbarAfter;
